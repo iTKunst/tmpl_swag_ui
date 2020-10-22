d3aa8d7e6311cfd3e29f3272f5128002cfaa50bb
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-22 11:53:03 -0400

    aa

--- docker/linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2086
-source LOG.sh
-
-log_enter /Project/docker/linux
-
-cp ./Project/docker/linux/*.sh ./Exe
-
-log_exit /Project/docker/linux
--- env/linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2086
-source LOG.sh
-
-log_enter /Project/env/linux
-
-cp ./Project/env/linux/*.sh ./Env
-
-log_exit /Project/env/linux
