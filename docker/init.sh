d45556eccdf1adca2f0281cc05121d63a0a710a1
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-22 12:39:19 -0400

    aa

--- docker/init.sh
+++ docker/init.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2086
 source LOG.sh
 
-log_enter /Project/docker/linux
+log_enter /Project/docker/init
 
 cp ./Project/docker/linux/*.sh ./Exe
 
-log_exit /Project/docker/linux
+log_exit /Project/docker/init
