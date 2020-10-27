9151423bd780efb88bbba6668544bb153d06b143
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-27 14:22:55 -0400

    aa

--- docker/init.cmd
+++ docker/init.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-call LOG_ENTER \DIR_PROJ\docker\init
+call LOG_ENTER \%DIR_PROJ%\docker\init
 
-xcopy /q /y .\DIR_PROJ\docker\win\*.cmd .\Exe
+xcopy /q /y .\%DIR_PROJ%\docker\win\*.cmd .\Exe
 
-call LOG_EXIT \DIR_PROJ\docker\init
+call LOG_EXIT \%DIR_PROJ%\docker\init
