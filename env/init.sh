9bd355cb31c06a6b62d79ac3da39a5a14105a6f2
TAGHRdooleyp <pjldooley@gmail.com>
2021-02-19 13:15:59 -0500

    ss

--- env/init.sh
+++ env/init.sh
@@ -6,12 +6,4 @@ log_enter /$DIR_TMPL/env/init
 
 cp ./$DIR_TMPL/env/linux/pENV.sh ./bin
 
-if [ ! -d ./project ]; then
-  mkdir project
-fi
-
-if [ ! -f ./project/pENV_MOD.sh ]; then
-  cp ./$DIR_TMPL/env/linux/pENV_MOD.sample.sh ./project/pENV_MOD.sh
-fi
-
 log_exit /$DIR_TMPL/env/init
